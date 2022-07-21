ffe3b97bcd57ad5db1f5aeb2e386e460c5b10510
HammadSiddique <hammad.siddique01@gmail.com>
2022-07-21 20:55:18 +0500

    Add 3 spaces in split method

--- morse_code.rb
+++ morse_code.rb
@@ -17,7 +17,7 @@ def decode_word(word)
 end
 
 def decode_message(message)
-  new_message = message.split('  ')
+  new_message = message.split('   ')
   message_result = new_message.map { |word| decode_word(word) }
   message_result.join(' ')
 end
